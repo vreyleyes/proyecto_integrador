2e5ac3db975bc5ef1db0ea9f0c9bce517d5e8b10
josefinabayugar <jbayugar@udesa.edu.ar>
2022-11-18 12:29:53 -0300

    probando busqueda

--- js/search-results.js
+++ js/search-results.js
@@ -1,5 +1,4 @@
 
-
 let query = location.search; // obtengo la QS
 let stringToObject = new URLSearchParams(query); // la transformo a un objeto literal 
 let aBuscar = stringToObject.get('búsqueda');  // obtengo los datos de "busqueda"
@@ -9,34 +8,43 @@ console.log(aBuscar);
 
 let api_key = "b91fa509ab378b2c4cee3ff42956d489"
 
-// let resultados = `https://api.themoviedb.org/3/search/multi?api_key=${api_key}&language=en-US&query=${aBuscar}&page=1&include_adult=false`;
-
 let resultados = `https://api.themoviedb.org/3/search/multi?api_key=${api_key}&language=en-US&query=${aBuscar}&page=1&include_adult=false`;
 
 console.log(resultados);
   
 
-// fetch(resultados)
+fetch(resultados)
 
-//     .then(function(response){
-//         return response.json()
-//     })
+    .then(function(response){
+        return response.json()
+    })
 
-//     .then(function(data){
-//         console.log(data);
-//         let info = data.results
-//         let conteiner = document.querySelector(".searchResults");
-//         let peliculas = '' // preguntar esto 
+    .then(function(data){
+        console.log(data);
+        let prueba = document.querySelector(".search");
+        let peliculaSearch = document.querySelector(".peliculaSearch")
+        let buscar = data.results          // o probar .data 
 
-//         for(let i=0; i<info.length; i ++){
-//             peliculas += `<article> `  // pregunar y terminar de agrgar la info. Hay que poner los detalles tmb.                     
+        if(buscar.length == 0){
+            probar.innerHTML = "No se encontro ningun resultado que coincide con "+ " "+ aBuscar
+        } else {
+            probar.innerHTML = "Estos son todos los resultados para" + " " + aBuscar
+        }
 
-//         }
-//     })
+        for (let i= 0; i< buscar.length; i++){
+            peliculaSearch.innerHTML += `        
+            <article>
 
-//     .catch(function(error){
-//         console.log(error);
-//     })
+
+            </article>`
+                   // agregar la info del article. 
+        } 
+
+    })
+
+    .catch(function(error){
+        console.log(error);
+    })
 
 
 
