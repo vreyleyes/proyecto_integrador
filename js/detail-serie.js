269f33973994be4976e77965fa639d8b3257ba7f
josefinabayugar <jbayugar@udesa.edu.ar>
2022-11-21 09:43:52 -0300

    detalle-serie

--- js/detail-serie.js
+++ js/detail-serie.js
@@ -9,25 +9,24 @@ let contenedor_detalle_serie = document.querySelector(".contenedor_detalle_serie
 
 ///// fetch "detalle de serie":
 
+
 fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        console.log(contenedor_detalle_serie);
-        let titulodedetalle = document.querySelector(".titulodedetalle")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let titulodedetalle = document.querySelector(".titulodedetalle");
 
-        titulodedetalle.innerText = data.name
+    titulodedetalle.innerText = data.name;
 
-        let generos2string = ""
+    let generos2string = "";
 
-        for (let i = 0; i < data.genres.length; i++) {
-            generos2string += data.genres[i].name + " "
-        }
+    for (let i = 0; i < data.genres.length; i++) {
+      generos2string += data.genres[i].name + " ";
+    }
 
-        contenedor_detalle_serie.innerHTML = `
-            <article class="item2">
+    let item2 = document.querySelector(".item2");
+    item2.innerHTML = `
                 <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}">
                 <aside>
                     <p>Estreno: ${data.first_air_date}</p>
@@ -36,101 +35,230 @@ fetch(url)
                     <p>Sinopsis: ${data.overview}</p>
                     <button type="button" class="favboton"><a class="favslink" href="./favorite.html">Agregar a Favoritos</a></button>
                 </aside>
-            </article>
-            <button class="mostrar">
-            Mostrar
-            </button>`
-    })
+            `;
+  });
 
-    fetch(`https://api.themoviedb.org/3/tv/${id_serie}/recommendations?api_key=${api_key}`) /// fetch recomendaciones 
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        contenedor_detalle_serie.innerHTML += 
-        `<section class="recomendaciones">
-        
-        </section>"`
-        let recomendaciones = document.querySelector(".recomendaciones")
-        
-        for (let i = 0; i < 5; i++) { 
+fetch(`https://api.themoviedb.org/3/tv/${id_serie}/recommendations?api_key=${api_key}`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let recomendaciones = document.querySelector(".recomendaciones");
 
-        recomendaciones.innerHTML += 
-        `<article>
+    for (let i = 0; i < 5; i++) {
+      recomendaciones.innerHTML += `
+        <article>
             <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
-        </article>`
+        </article>
+        `;
+    }
 
+    // boton pero no funciona muy bien (revisar)
+    let boton = document.querySelector(".toggle");
+    console.log(boton);
+    boton.addEventListener("click", function () {
+      let recomendaciones = document.querySelector(".recomendaciones")
+      recomendaciones.classList.toggle("not-show")
+      if (boton.value === "Mostrar") {
+        boton.innerText = "Ocultar"
+      } else {
+        boton.innerText = "Mostrar"
+      }
+      console.log("HOLA MUNDO");
+    });
+
+
+  });
+
+fetch(
+  `https://api.themoviedb.org/3/tv/${id_serie}/watch/providers?api_key=${api_key}`
+) /// APARECE ANTES DE RECOMENACIONES, tiene que estar después
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+    let comprar = document.querySelector(".comprar");
+    let streaming = document.querySelector(".streaming");
+
+    if (data.results.US) {
+      if (data.results.US.buy) {
+        for (let i = 0; i < data.results.US.buy.length; i++) {
+          comprar.innerHTML += `
+          <article>
+            <img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.US.buy[i].logo_path}" alt="">
+            <p>${data.results.US.buy[i].provider_name}</p>
+          </article>`;
         }
-        console.log(data);
+      }
+      if (data.results.US.flatrate) {
+        for (let i = 0; i < data.results.US.flatrate.length; i++) {
+          streaming.innerHTML += `
+          <article>
+          <img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.US.flatrate[i].logo_path}" alt=""><p>${data.results.US.flatrate[i].provider_name}</p>
+          </article>`;
+        }
+      }
+    }
+    if (data.results.CH) {
+      if (data.results.CH.buy) {
+        for (let i = 0; i < data.results.CH.buy.length; i++) {
+          comprar.innerHTML += `
+          <article>
+          <img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.CH.buy[i].logo_path}" alt=""><p>${data.results.CH.buy[i].provider_name}</p>
+          </article>`;
+        }
+      }
+      if (data.results.CH.flatrate) {
+        for (let i = 0; i < data.results.CH.flatrate.length; i++) {
+          streaming.innerHTML += `
+          <article>
+          <img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.CH.flatrate[i].logo_path}" alt=""><p>${data.results.CH.flatrate[i].provider_name}</p>
+          </article>`;
+        }
+      }
+    }
+  })
 
-        // boton pero no funciona muy bien (revisar)
-        let boton = document.querySelector(".mostrar")
-        boton.addEventListener("click", function () {
-            let recomendaciones = document.querySelector(".recomendaciones")
-            console.log(recomendaciones.style.display);
-            if (recomendaciones.style.display == "none") {
-                recomendaciones.style.display = "block"
-                boton.innerText = "ocultar"
-            } else {
-                recomendaciones.style.display = "none"
-                boton.innerText = "mostrar"
-            }
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch(url)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//         console.log(contenedor_detalle_serie);
+//         let titulodedetalle = document.querySelector(".titulodedetalle")
+
+//         titulodedetalle.innerText = data.name
+
+//         let generos2string = ""
+
+//         for (let i = 0; i < data.genres.length; i++) {
+//             generos2string += data.genres[i].name + " "
+//         }
+
+//         contenedor_detalle_serie.innerHTML = `
+//             <article class="item2">
+//                 <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}">
+//                 <aside>
+//                     <p>Estreno: ${data.first_air_date}</p>
+//                     <p>Calificación: ${data.vote_average}</p>
+//                     <p>Género: ${generos2string}</p>
+//                     <p>Sinopsis: ${data.overview}</p>
+//                     <button type="button" class="favboton"><a class="favslink" href="./favorite.html">Agregar a Favoritos</a></button>
+//                 </aside>
+//             </article>
+//             <button class="mostrar">
+//             Mostrar
+//             </button>`
+//     })
+
+//     fetch(`https://api.themoviedb.org/3/tv/${id_serie}/recommendations?api_key=${api_key}`) /// fetch recomendaciones 
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         contenedor_detalle_serie.innerHTML += 
+//         `<section class="recomendaciones">
+        
+//         </section>"`
+//         let recomendaciones = document.querySelector(".recomendaciones")
+        
+//         for (let i = 0; i < 5; i++) { 
+
+//         recomendaciones.innerHTML += 
+//         `<article>
+//             <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
+//         </article>`
+
+//         }
+//         console.log(data);
+
+//         // boton pero no funciona muy bien (revisar)
+//         let boton = document.querySelector(".mostrar")
+//         boton.addEventListener("click", function () {
+//             let recomendaciones = document.querySelector(".recomendaciones")
+//             console.log(recomendaciones.style.display);
+//             if (recomendaciones.style.display == "none") {
+//                 recomendaciones.style.display = "block"
+//                 boton.innerText = "ocultar"
+//             } else {
+//                 recomendaciones.style.display = "none"
+//                 boton.innerText = "mostrar"
+//             }
            
-        })
-    })
+//         })
+//     })
 
-    fetch(`https://api.themoviedb.org/3/tv/${id_serie}/watch/providers?api_key=${api_key}`) /// APARECE ANTES DE RECOMENACIONES, tiene que estar después
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        contenedor_detalle_serie.innerHTML +=
-        `<section class="comprar"></section>
-        <section class="streaming">
-            <img src="" alt="">
-            <p></p>
-        </section>`
-        let comprar = document.querySelector(".comprar")
-        let streaming = document.querySelector(".streaming")
+//     fetch(`https://api.themoviedb.org/3/tv/${id_serie}/watch/providers?api_key=${api_key}`) /// APARECE ANTES DE RECOMENACIONES, tiene que estar después
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//         contenedor_detalle_serie.innerHTML +=
+//         `<section class="comprar"></section>
+//         <section class="streaming">
+//             <img src="" alt="">
+//             <p></p>
+//         </section>`
+//         let comprar = document.querySelector(".comprar")
+//         let streaming = document.querySelector(".streaming")
 
-        if (data.results.US) {
-            if (data.results.US.buy) {
-                comprar.innerHTML += '<h3 class="elmismotitulo">Comprar</h3>'
-                for (let i = 0; i < data.results.US.buy.length; i++) {
-                    comprar.innerHTML += `<img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.US.buy[i].logo_path}" alt="">
-                    <p>${data.results.US.buy[i].provider_name}</p>`
-                }
-            }
-            if (data.results.US.flatrate) {
-                streaming.innerHTML += '<h3 class="elmismotitulo">Streaming</h3>'
-                for (let i = 0; i < data.results.US.flatrate.length; i++) {
-                    streaming.innerHTML += `<img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.US.flatrate[i].logo_path}" alt="">
-                    <p>${data.results.US.flatrate[i].provider_name}</p>`
-                }
-            }
-        }
-        if (data.results.CH) {
-            if (data.results.CH.buy) {
-                comprar.innerHTML += '<h3 class="elmismotitulo">Comprar</h3>'
-                for (let i = 0; i < data.results.CH.buy.length; i++) {
-                    comprar.innerHTML += `<img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.CH.buy[i].logo_path}" alt="">
-                    <p>${data.results.CH.buy[i].provider_name}</p>`
-                }
-            }
-            if (data.results.CH.flatrate) {
-                streaming.innerHTML += '<h3 class="elmismotitulo">Streaming</h3>'
-                for (let i = 0; i < data.results.CH.flatrate.length; i++) {
-                    streaming.innerHTML += `<img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.CH.flatrate[i].logo_path}" alt="">
-                    <p>${data.results.CH.flatrate[i].provider_name}</p>`
-                }
-            }
-        }
-    })
+//         if (data.results.US) {
+//             if (data.results.US.buy) {
+//                 comprar.innerHTML += '<h3 class="elmismotitulo">Comprar</h3>'
+//                 for (let i = 0; i < data.results.US.buy.length; i++) {
+//                     comprar.innerHTML += `<img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.US.buy[i].logo_path}" alt="">
+//                     <p>${data.results.US.buy[i].provider_name}</p>`
+//                 }
+//             }
+//             if (data.results.US.flatrate) {
+//                 streaming.innerHTML += '<h3 class="elmismotitulo">Streaming</h3>'
+//                 for (let i = 0; i < data.results.US.flatrate.length; i++) {
+//                     streaming.innerHTML += `<img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.US.flatrate[i].logo_path}" alt="">
+//                     <p>${data.results.US.flatrate[i].provider_name}</p>`
+//                 }
+//             }
+//         }
+//         if (data.results.CH) {
+//             if (data.results.CH.buy) {
+//                 comprar.innerHTML += '<h3 class="elmismotitulo">Comprar</h3>'
+//                 for (let i = 0; i < data.results.CH.buy.length; i++) {
+//                     comprar.innerHTML += `<img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.CH.buy[i].logo_path}" alt="">
+//                     <p>${data.results.CH.buy[i].provider_name}</p>`
+//                 }
+//             }
+//             if (data.results.CH.flatrate) {
+//                 streaming.innerHTML += '<h3 class="elmismotitulo">Streaming</h3>'
+//                 for (let i = 0; i < data.results.CH.flatrate.length; i++) {
+//                     streaming.innerHTML += `<img class="imgdata" src="https://image.tmdb.org/t/p/w200/${data.results.CH.flatrate[i].logo_path}" alt="">
+//                     <p>${data.results.CH.flatrate[i].provider_name}</p>`
+//                 }
+//             }
+//         }
+//     })
 
-    .catch(function (error) {
-        console.log(error);
-    })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
 
 
 
